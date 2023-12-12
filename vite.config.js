ea395eae3c33da6336fd0e50303ad23bb6690924
mohamed yassin <mohamedyas8295@gmail.com>
2023-12-12 20:04:46 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import eslintPlugin from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslintPlugin()],
-  // base: "/React-WorldWise-app/",
+  base: "/React-WorldWise-app/",
 });
