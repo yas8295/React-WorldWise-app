34362157e99e75d1a92110f5ed70ea3eb2a3bdcd
mohamed yassin <mohamedyas8295@gmail.com>
2023-12-12 20:01:18 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ActiveUser } from "./Context/ActiveUser";
 import { Context } from "./Context/Context";
-import { Suspense, lazy } from "react";
 import "./bootstrap-grid.min.css";
 import "./bootstrap-utilities.min.css";
 import "./bootstrap.css";
@@ -11,39 +10,36 @@ import Cities from "./Components/Cities";
 import Countries from "./Components/Countries";
 import City from "./Components/City";
 import Form from "./Components/Form";
-import LoadingFullPage from "./Components/LoadingFullPage";
-const Pricing = lazy(() => import("./Pages/Pricing"));
-const MainPage = lazy(() => import("./Pages/MainPage"));
-const Login = lazy(() => import("./Pages/Login"));
-const Application = lazy(() => import("./Pages/Application"));
-const Product = lazy(() => import("./Pages/Product"));
+import MainPage from "./assets/Pages/MainPage";
+import Pricing from "./assets/Pages/Pricing";
+import Product from "./assets/Pages/Product";
+import Login from "./Pages/Login";
+import Application from "./assets/Pages/Application";
 
 function App() {
   return (
     <Context>
       <BrowserRouter>
-        <Suspense fallback={<LoadingFullPage></LoadingFullPage>}>
-          <Routes>
-            <Route index element={<MainPage />} />
-            <Route path="Pricing" element={<Pricing />} />
-            <Route path="Product" element={<Product />} />
-            <Route path="Login" element={<Login />} />
-            <Route
-              path="Application"
-              element={
-                <ActiveUser>
-                  <Application />
-                </ActiveUser>
-              }
-            >
-              <Route index element={<Cities />} />
-              <Route path="cities" element={<Cities />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<Countries></Countries>} />
-              <Route path="form" element={<Form></Form>} />
-            </Route>
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route index element={<MainPage />} />
+          <Route path="Pricing" element={<Pricing />} />
+          <Route path="Product" element={<Product />} />
+          <Route path="Login" element={<Login />} />
+          <Route
+            path="Application"
+            element={
+              <ActiveUser>
+                <Application />
+              </ActiveUser>
+            }
+          >
+            <Route index element={<Cities />} />
+            <Route path="cities" element={<Cities />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<Countries></Countries>} />
+            <Route path="form" element={<Form></Form>} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </Context>
   );
